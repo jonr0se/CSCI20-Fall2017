9fb3ce6b783e7fac3554b680448ae31c27a33371
Jon <jrose012@student.butte.edu>
2017-08-23 19:01:06 +0000

    lab11.cpp

---
+++ lab11/lab11.cpp
@@ -0,0 +1,39 @@
+An algorithm for a simple number guessing game.
+
+Start with number 1 through 50 displayed on screen.
+
+The computer will choose a random number between 1 and 50.
+
+In this case, the computer chooses the number 1.
+
+An alert says: THE USER HAS 10 POINTS!
+
+The user guesses the number 50.
+
+An alert says: THE NUMBER IS LOWER!
+
+An alert says: THE USER HAS 9 POINTS!
+
+The user guesses the number 25.
+
+An alert says: THE NUMBER IS LOWER!
+
+An alert says: THE USER HAS 8 POINTS!
+
+The user guesses the number 5.
+
+An alert says: THE NUMBER IS LOWER!
+
+An alert says: THE USER HAS 7 POINTS!
+
+The user guesses the number 3.
+
+An alert says: THE NUMBER IS LOWER!
+
+An alert says: THE USER HAS 6 POINTS!
+
+The user correctly guesses the number 1.
+
+An alert says: THE USER'S GUESS WAS CORRECT!
+
+An alert says: THE USER FINISHED THE GAME WITH 5 POINTS!
