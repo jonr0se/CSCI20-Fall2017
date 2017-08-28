ccdcc19dd769ceabacdcb17a947eaca2e57841cf
Jon <jrose012@student.butte.edu>
2017-08-28 19:26:16 +0000

    lab12.cpp

---
+++ lab12/lab12.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    cout << "                        _ " << endl; 
+    cout << "                       | \\" << endl; 
+    cout << "                       | |" << endl; 
+    cout << "                       | |" << endl; 
+    cout << "                       | |" << endl; 
+    cout << "   |\\                  | |" << endl; 
+    cout << "  /, ~\\               / /" << endl; 
+    cout << " X     `-.....-------./ / " << endl; 
+    cout << "  ~-. ~  ~              | " << endl; 
+    cout << "     \\             /    | " << endl; 
+    cout << "      \\  /_     ___\\   /  " << endl; 
+    cout << "      | /\\ ~~~~~   \\ |" << endl; 
+    cout << "      | | \\        || |" << endl; 
+    cout << "      | |\\ \\       || )" << endl; 
+    cout << "      (_/ (_/      ((_/" << endl; 
+}
