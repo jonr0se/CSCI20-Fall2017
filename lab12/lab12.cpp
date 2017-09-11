a7a1f56380ee76b16762dbac61d3b7af0a6e206f
Jon <jrose012@student.butte.edu>
2017-09-11 20:12:47 +0000

    lab12

--- lab12/lab12.cpp
+++ lab12/lab12.cpp
@@ -1,3 +1,21 @@
+/* 
+Create a program that will output a picture made completely of characters.  
+This is often called ASCII art https://en.wikipedia.org/wiki/ASCII_art
+
+Your picture must have at least 5 lines of characters and make an actual picture of some sort.  Each line needs at least 2 characters output.
+
+No input is required for this assignment.
+
+Run your program through the Run button at the top of your workspace.
+*/
+//Jon Rose
+//April Browne
+//CSCI 020
+// 9/11/2017
+
+
+
+
 
 #include <iostream>
 using namespace std;
@@ -20,3 +38,26 @@ int main(){
     cout << "      | |\\ \\       || )" << endl; 
     cout << "      (_/ (_/      ((_/" << endl; 
 }
+
+
+/*
+Running /home/ubuntu/workspace/lab12/lab12.cpp
+                       __ 
+                       | \
+                       | |
+                       | |
+                       | |
+   |\                  | |
+  /, ~\               / /
+ X     `-.....-------./ / 
+  ~-. ~  ~              | 
+     \             /    | 
+      \  /_     ___\   /  
+      | /\ ~~~~~   \ |
+      | | \        || |
+      | |\ \       || )
+      (_/ (_/      ((_/
+
+
+Process exited with code: 0
+*/
