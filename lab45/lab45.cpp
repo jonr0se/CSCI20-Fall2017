d5647e2f3f23dc849b1487879e6fed580b5a2221
Jon <jrose012@student.butte.edu>
2017-11-16 07:56:27 +0000

    lab45

---
+++ lab45/lab45.cpp
@@ -0,0 +1,22 @@
+//Jon Rose
+//April Browne
+//CSCI 020
+//11/15/17
+/*
+The purpose of this lab is to create user names for a website. It should take a user input first name up to 10 characters.
+It should also take a user input last name up to 20 characters. It should tell the user if the name is larger than the max size.
+It should also determine if the names are the same and output a warning. It should create three different user name options.
+(for example: apbrowne; aprilbrowne; abrowne).
+*/
+
+
+    #include<iostream>
+    #include<cstring>
+    #include<cctype>
+    using namespace std;
+    
+    int main() {
+        
+            
+            
+            
