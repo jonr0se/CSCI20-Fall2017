53e94a5fda4e27fb1d1262f57e8bee8ea0d395e0
Jon <jrose012@student.butte.edu>
2018-03-07 19:00:18 +0000

    Assignment5

--- Assign5/Assignment5.cpp
+++ Assign5/Assignment5.cpp
@@ -12,43 +12,49 @@ The program should help us understand how programming can help in our field. */
     #include <iostream> // iostream
     #include <cmath> // math
     
-    using namespace std;
+    using namespace std; // using name space std
     
-    float calcSD(float data[]);
+    float calcSD(float data[]); // float calcSD float data
     
-    int main()
+    int main()  // int main
     {
-        int i;
-        float data[10];
+        int i;  // int i
+        int middle; // int middle
+        float data[10]; // float data 10 (could be more? 15? 20? 25? etc)
+        double average, median;  // double average median
         
-        cout << "Enter 10 numbers to calculate standard deviation: ";
+       
+        cout << "Enter 10 numbers to calculate standard deviation: "; // outputs this text
         
-        for(i = 0; i < 10; ++i)
-        cin >> data[i];
+        for(i = 0; i < 10; ++i)  // one for loop
+        cin >> data[i];  // allows data input i 
         
-        cout << endl << "Standard Deviation = " << calcSD(data);
+        cout << endl << "Standard Deviation = " << calcSD(data);  // outputs text along with calcSD and data
+        
 
         return 0;
         }
         
-        float calcSD(float data[])
+        float calcSD(float data[]) // float calcSD float data
         {
             
-            float sum = 0.0, mean, standardDeviation = 0.0;
-        
+            float sum = 0.0, mean, standardDeviation = 0.0;  // float sum = 0.0 mean standard deviation = 0.0 
             
-            int i;
             
-            for(i = 0; i < 10; ++i)
+            int i;  // int i
+            
+            for(i = 0; i < 10; ++i)  // for loop
         {
-            sum += data[i];
+            sum += data[i];   // adding sum and data of i
         }
-            mean = sum/10;
+            mean = sum/10;  // calculates mean takes sum and divides by 10 in this case
+            cout << "Mean = " << mean;  // outputs text an mean value
+            for(i = 0; i < 10; ++i)  // for loop
+            standardDeviation += pow(data[i] - mean, 2);  //  standard deviation calculation
             
-            for(i = 0; i < 10; ++i)
-            standardDeviation += pow(data[i] - mean, 2);
+            return sqrt(standardDeviation / 10); // calculation 
             
-            return sqrt(standardDeviation / 10);
+            
         }
         
         /*
